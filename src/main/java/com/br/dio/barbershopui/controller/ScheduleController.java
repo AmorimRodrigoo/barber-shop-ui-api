efb83be0800d2d7ca94362b2c253f78cb6f62ffc
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-21 18:27:03 -0300

    ajustes

--- src/main/java/com/br/dio/barbershopui/controller/ScheduleController.java
+++ src/main/java/com/br/dio/barbershopui/controller/ScheduleController.java
@@ -2,19 +2,17 @@ package com.br.dio.barbershopui.controller;
 
 import com.br.dio.barbershopui.controller.request.SaveScheduleRequest;
 import com.br.dio.barbershopui.controller.response.SaveScheduleResponse;
+import com.br.dio.barbershopui.controller.response.ScheduleAppointmentMonthResponse;
 import com.br.dio.barbershopui.mapper.IScheduleMapper;
 import com.br.dio.barbershopui.services.IScheduleService;
 import com.br.dio.barbershopui.services.query.IScheduleQueryService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.YearMonth;
+
+import static java.time.ZoneOffset.UTC;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
@@ -38,5 +36,18 @@ public class ScheduleController {
     @ResponseStatus(NO_CONTENT)
     void delete(@PathVariable final long id){
         service.delete(id);
+    }
+
+    @GetMapping("{year}/{month}")
+    ScheduleAppointmentMonthResponse listMonth(@PathVariable final int year, @PathVariable final int month){
+        var yearMonth = YearMonth.of(year, month);
+        var startAt = yearMonth.atDay(1)
+                .atTime(0, 0, 0, 0)
+                .atOffset(UTC);
+        var endAt = yearMonth.atEndOfMonth()
+                .atTime(23, 59, 59, 999_999_999)
+                .atOffset(UTC);
+        var entities = queryService.findInMonth(startAt, endAt);
+        return mapper.toMonthResponse(year, month, entities);
     }
 }
--- src/main/java/com/br/dio/barbershopui/controller/response/ScheduleAppointmentMonthResponse.java
+++ src/main/java/com/br/dio/barbershopui/controller/response/ScheduleAppointmentMonthResponse.java
@@ -1,9 +1,13 @@
 package com.br.dio.barbershopui.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record ScheduleAppointmentMonthResponse(
+        @JsonProperty("year")
         int year,
+        @JsonProperty("month")
         int month,
         List<ClientScheduleAppointmentResponse> scheduledAppointments
 ) {}
